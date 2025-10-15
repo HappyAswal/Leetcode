46ac76493e9fcf5c44297d924b5159289d0a6a32
HappyAswal <uhmhappy1@gmail.com>
2025-10-15 23:51:39 +0530

    Time: 0 ms (100%), Space: 10.4 MB (81.83%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minLength(string s) {
+        string ans="";
+        for(char c: s){
+            if(ans.size() && 
+            ((ans.back() == 'A' && c == 'B') || (ans.back() == 'C' && c == 'D'))){
+                ans.pop_back();
+            }else ans.push_back(c);
+        }
+        return ans.size();
+    }
+};
