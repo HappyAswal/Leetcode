993c4900552f3258c92ab3883c68bc1e14f8cdf7
HappyAswal <uhmhappy1@gmail.com>
2025-12-26 16:39:48 +0530

    Time: 15 ms (17.6%), Space: 21.6 MB (45.57%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int penalty(string& customers,int i){
-        int penalty=0;
-        for(int j=0;j<i;j++){
-            if(customers[j]=='N') penalty+=1;
-        }
-        for(int k=i;k<customers.size();k++){
-            if(customers[k]=='Y') penalty+=1;
-        }
-        return penalty;
-    }
+
     int bestClosingTime(string customers) {
-        int n = customers.size();
-        int ans = 0;
-        int minPenalty = INT_MAX;
+        int n=customers.size();
+        vector<int> prefixN(n+1,0); 
+        prefixN[0]=0; // 0 se phli kitni penalty
+        vector<int> suffixY(n+1,0);
+        suffixY[n]=0; // last time ke bad kitni penalty
+        for(int i=1;i<=n;i++){
+            if(customers[i-1]=='N') prefixN[i]=prefixN[i-1]+1;
+            else prefixN[i]=prefixN[i-1];
+        }
+        for(int i= n-1;i>=0;i--){
+            if(customers[i]=='Y') suffixY[i]=suffixY[i+1]+1;
+            else suffixY[i]=suffixY[i+1];
+        }
+        int mP=INT_MAX,mH=INT_MAX;
         for(int i=0;i<=n;i++){
-            int p=penalty(customers,i);
-            if(p<minPenalty){
-                minPenalty=p;
-                ans=i;
+            int cP=prefixN[i]+suffixY[i];
+            if(cP<mP){
+                mP=cP;
+                mH=i;
             }
         }
-        return ans;
+        return mH;
     }
 };
