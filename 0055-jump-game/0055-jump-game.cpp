9ef853d368d4b38ba6057a780b02f9b06d1ba304
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-01-09 22:39:48 +0530

    Time: 0 ms (100%), Space: 52.3 MB (35.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    vector<int>dp;
-    bool fxn(int i,vector<int>&nums){
-        if(i>=nums.size()-1) return true ;
-        if(dp[i]!=-1) return dp[i];
-        for(int j=1;j<=nums[i];j++){
-            if(fxn(i+j,nums)) return dp[i]=1; 
-        }
-        return dp[i]=0;
-    }
     bool canJump(vector<int>& nums) {
-        dp.assign(nums.size(),-1);
-        return fxn(0,nums);
+        int maxreach=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxreach) return false;
+            maxreach=max(maxreach,i+nums[i]);
+        }
+        return true;
     }
 };
