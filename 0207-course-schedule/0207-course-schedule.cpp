e6a3b3594c2c3cabc68de172924be6b534be7639
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-02-14 15:13:38 +0530

    Time: 0 ms (100%), Space: 19.5 MB (50.4%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+        for(auto&x:prerequisites){
+            int u=x[0];
+            int v=x[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+
+        vector<int>topo;
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int f=q.front();
+            topo.push_back(f);
+            q.pop();
+            for(auto&nbr:adj[f]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.push(nbr);
+            }
+        }
+        return topo.size()==numCourses;
+    }
+};
