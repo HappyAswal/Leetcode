5abd7bfcc48ddb6b0ee8d4665c9b145e6758885a
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-02-27 10:34:00 +0530

    Time: 2 ms (24.64%), Space: 8.5 MB (81.75%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<int>dp;
-    int solve(int i,string& s){
-        if(i==s.size()) return dp[i]=1;
-        if(dp[i]!=-1) return dp[i];
-        if(s[i]=='0') return dp[i]=0;
-        int ith=solve(i+1,s);
-        int i_i1th=0;
-        if(i+1<s.size()){
-            if(s[i]=='1'||(s[i]=='2' && s[i+1]<='6' )){
-                i_i1th=solve(i+2,s);
-            }
-        }
-        return dp[i]=ith + i_i1th;
-    }
     int numDecodings(string s) {
         int n=s.size();
-        dp.assign(101,-1);
-        return solve(0,s);
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0') dp[i]=0;
+            else{
+                dp[i]=dp[i+1];
+                if(i+1<n){
+                    if(s[i]=='1'||(s[i]=='2'&& s[i+1]<='6')){
+                        dp[i]+=dp[i+2];
+                    }
+                }
+            }
+        }
+        return dp[0];
     }   
 };
