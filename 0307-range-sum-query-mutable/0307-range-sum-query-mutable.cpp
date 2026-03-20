21475c92bb54422a3341c11d42e002de36facdb0
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-03-20 22:51:06 +0530

    Time: 66 ms (44.37%), Space: 181.5 MB (65.24%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -0,0 +1,54 @@
+
+class NumArray {
+public:
+    int n;
+    int seg[4*3*10001];
+    void build(int idx,int l,int r,vector<int>&nums){
+        if(l==r){
+            seg[idx]=nums[l];
+            return;
+        }
+        int mid=l+(r-l)/2;
+        build(2*idx+1,l,mid,nums);
+        build(2*idx+2,mid+1,r,nums);
+        seg[idx]=seg[2*idx+1]+seg[2*idx+2];
+    }
+    void updatefxn(int idx,int l,int r,int range,int nv){
+        if(l==r){
+            seg[idx]=nv;
+            return;
+        }
+        int mid=l+(r-l)/2;
+        if(range<=mid) updatefxn(2*idx+1,l,mid,range,nv);
+        else updatefxn(2*idx+2,mid+1,r,range,nv);
+        seg[idx]=seg[2*idx+1]+seg[2*idx+2];
+    }
+    int query(int idx,int l,int r,int ql,int qr){
+        if(r<ql || l>qr) return 0;
+        if(l>=ql && r<=qr) return seg[idx];
+
+        int mid=l+(r-l)/2;
+        int left=query(2*idx+1,l,mid,ql,qr);
+        int right=query(2*idx+2,mid+1,r,ql,qr);
+        return left+right;
+    }
+    NumArray(vector<int>& nums) {
+        n=nums.size();
+        build(0,0,n-1,nums);
+    }
+    
+    void update(int index, int val) {
+        updatefxn(0,0,n-1,index,val);
+    }
+    
+    int sumRange(int left, int right) {
+        return query(0,0,n-1,left,right);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
