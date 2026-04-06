ff95b908680fe61003691514321f9c0242ff32d7
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-06 16:33:53 +0530

    Time: 111 ms (37.04%), Space: 106.7 MB (22.24%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    //dfs1
+    vector<vector<int>>adj;
+    vector<int>dp;
+    vector<int>sub;
+    vector<int>res;
+    int nn;
+    void dfs2(int node,int parent){
+        for(auto&v:adj[node]){
+            if(v!=parent){
+                res[v]=res[node]+(nn-sub[v])-sub[v];
+                dfs2(v,node);
+            }
+        }
+    }
+    void dfs1(int node,int parent){
+        dp[node]=0;
+        sub[node]=1;
+        for(int v:adj[node]){
+            if(v!=parent){
+                dfs1(v,node);
+                sub[node]+=sub[v];
+                dp[node]+=(dp[v]+sub[v]);
+            }
+        }
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        nn=n;
+        dp.assign(n,0);
+        sub.assign(n,1);
+        adj.resize(n);
+        res.resize(n);
+
+        for(auto x:edges){
+            int a=x[0],b=x[1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        dfs1(0,-1);
+        res[0]=dp[0];
+        dfs2(0,-1);
+        return res;
+    }
+};
