ecf38aa4f5f3b1dc894a78770aa1feb3b77c6021
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-16 01:46:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int nxtg(int i,vector<int>& nums2){
-        for(int j=i+1;j<nums2.size();j++){
-            if(nums2[j]>nums2[i]) return nums2[j];
-        }
-        return -1;
-    }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size(),n2=nums2.size();
-        vector<int> ans;
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(nums1[i]==nums2[j]){
-                    ans.push_back(nxtg(j,nums2));
-                }
+        stack<int>s;
+        unordered_map<int,int>ng;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(s.size()>0 && nums2[i]>s.top()){
+                s.pop();
             }
+            if(s.size()) ng[nums2[i]]=s.top();
+            else ng[nums2[i]]=-1;
+            s.push(nums2[i]);
+        }
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(ng[nums1[i]]);
         }
         return ans;
     }
