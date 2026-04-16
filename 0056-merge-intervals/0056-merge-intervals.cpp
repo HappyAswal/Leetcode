76be5b698b694d8eadc3157fd2546f01788ce817
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-16 22:46:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& iv) {
-        sort(iv.begin(),iv.end());
-        int n=iv.size();
-        vector<int> comp=iv[0];
+    vector<vector<int>> merge(vector<vector<int>>& inl) {
+        sort(inl.begin(),inl.end());
+        vector<int>compare=inl[0];
         vector<vector<int>>ans;
-        for(int i=1;i<n;i++){
-            if(iv[i][0]<=comp[1]){
-                //overlap
-                comp[1]=max(comp[1],iv[i][1]);
+        for(int i=1;i<inl.size();i++){
+            if(inl[i][0]<=compare[1]){
+                compare[1]=max(compare[1],inl[i][1]);
             }else{
-                ans.push_back(comp);
-                comp=iv[i];
+                ans.push_back(compare);
+                compare=inl[i];
             }
         }
-        ans.push_back(comp);
+        ans.push_back(compare);
         return ans;
     }
 };
