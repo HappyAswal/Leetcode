894786d065ae7e15ff2183798dcfd0a93173dee2
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-16 23:03:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int mcoin(int i,int amount,vector<int>& coins){
-        if(amount==0) return 0;
+    int solve(int i,vector<int>&coins,int amount){
         if(i>=coins.size()) return 1e9;
+        if(amount==0) return 0;
         if(dp[i][amount]!=-1) return dp[i][amount];
         int pick=1e9;
-        if(coins[i]<=amount) pick=1+mcoin(i,amount-coins[i],coins);
-        int notpick=mcoin(i+1,amount,coins);
+        if(coins[i]<=amount){
+            pick=1+solve(i,coins,amount-coins[i]);
+        }
+        int notpick=solve(i+1,coins,amount);
         return dp[i][amount]=min(pick,notpick);
     }
     int coinChange(vector<int>& coins, int amount) {
         dp.assign(coins.size(),vector<int>(amount+1,-1));
-        int ans=mcoin(0,amount,coins);
-        return (ans>=1e9) ? -1:ans;
+        int ans=solve(0,coins,amount); 
+        return (ans>=1e9)?-1:ans;
     }
 };
