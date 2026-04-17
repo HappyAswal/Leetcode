a9fa8aa6e4c6bcd272b8636692628a5cd8fe8527
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-17 16:05:42 +0530

    Time: 0 ms (100%), Space: 23.7 MB (98.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
+        int idx=0;
         int n=nums.size();
-        while(i<n){
-            if(nums[i]!=0) i++;
-            else{
-                //find next non zero
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]!=0){
-                        swap(nums[i],nums[j]);
-                        break;
-                    }
-                }
-                i++;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[idx]);
+                idx++;
             }
         }
     }
