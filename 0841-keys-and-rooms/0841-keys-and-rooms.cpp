659a4a84d92a7e0f8717e180c247ea638118619f
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-17 23:52:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++){
-            for(int x:rooms[i]){
-                adj[i].push_back(x);
-            }
-        }
         vector<int>vis(n,false);
         vis[0]=true;
         queue<int>q;
         q.push(0);
         while(!q.empty()){
             int node=q.front(); q.pop();
-            if(adj[node].size()==0) vis[node]=true;
-            for(int nbr:adj[node]){
+            for(int nbr:rooms[node]){
                 if(!vis[nbr]){
                     q.push(nbr);
                     vis[nbr]=true;
