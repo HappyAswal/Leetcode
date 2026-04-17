54b488148ae573b655cdd774fbb451098e135fe3
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-17 16:57:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxreach=0;
+        int maxj=0;
         for(int i=0;i<nums.size();i++){
-            if(i>maxreach) return false;
-            maxreach=max(maxreach,i+nums[i]);
+            if(i>maxj) return false;
+            maxj=max(maxj,i+nums[i]);
         }
-        return true;
+        return 0==0;
     }
 };
