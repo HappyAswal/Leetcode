b7358c95b70b6f38579777d4be58cfba7989606b
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-17 23:20:12 +0530

    Time: 0 ms (100%), Space: 17.9 MB (98.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int n, m;
-    int oC, nC;
-
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        n = image.size();
-        m = image[0].size();
-        oC = image[sr][sc];
-        nC = color;
-        if (oC == nC) return image;
-        dfs(sr, sc, image);
-        return image;
+    int dx[4]={0,0,-1,1};
+    int dy[4]={1,-1,0,0};
+    int src_color;
+    int clr;
+    void dfs(int i,int j,vector<vector<int>>& image){
+        image[i][j]=clr;
+        for(int k=0;k<4;k++){
+            int ni=i+dx[k];
+            int nj=j+dy[k];
+            if(ni>=0 && nj>=0 && ni<image.size() && nj<image[0].size() && image[ni][nj]==src_color){
+                dfs(ni,nj,image);
+            }
+        }
     }
-
-    void dfs(int x, int y, vector<vector<int>>& image) {
-        if (x < 0 || y < 0 || x >= n || y >= m) return;
-        if (image[x][y] != oC) return;
-        image[x][y] = nC;
-        dfs(x - 1, y, image);
-        dfs(x + 1, y, image);
-        dfs(x, y - 1, image);
-        dfs(x, y + 1, image);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        src_color=image[sr][sc];
+        clr=color;
+        if(src_color==clr) return image;
+        dfs(sr,sc,image);
+        return image;
     }
 };
