8caff1a9505156273616f5041f65e87624377f04
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-18 16:08:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -13,6 +13,7 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>>ans;
+        if(root==NULL) return ans;
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
@@ -20,13 +21,11 @@ public:
             vector<int>temp;
             for(int i=0;i<n;i++){
                 TreeNode* node=q.front(); q.pop();
-                if(node!=NULL){
-                    temp.push_back(node->val);
-                    q.push(node->left);
-                    q.push(node->right);
-                }
+                temp.push_back(node->val);
+                if(node->left)  q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-            if(!temp.empty()) ans.push_back(temp);
+            ans.push_back(temp);
         }
         return ans;
     }
