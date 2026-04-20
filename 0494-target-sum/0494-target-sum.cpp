b6cbeae3fa6c83bacd173ce8570cbaf880a05391
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-20 16:02:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+    int total;
     int solve(int i,vector<int>&nums,int sum,int target){
-        if(target==sum && i==nums.size()) return 1;
-        if(target!=sum && i==nums.size()) return 0;
+        if(i==nums.size()) return target==sum;
+        if(dp[i][sum+total]!=-1) return dp[i][sum+total];
         int pickp=solve(i+1,nums,sum+nums[i],target);
         int pickm=solve(i+1,nums,sum-nums[i],target);
-        return pickp+pickm;
+        return dp[i][sum+total]=pickp+pickm;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        total=accumulate(nums.begin(),nums.end(),0);
+        dp.assign(nums.size(),vector<int>(2*total+1,-1));
         return solve(0,nums,0,target);
     }
 };
