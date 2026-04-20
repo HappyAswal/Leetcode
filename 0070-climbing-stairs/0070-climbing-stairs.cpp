d5aa150d46da1a6afa646133bf2c822c595e0243
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-20 13:21:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,6 +3,9 @@ public:
     int climbStairs(int n) {
         vector<int>dp(n+1);
         if(n<=3) return n;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
         for(int i=4;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
