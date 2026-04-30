30e9b5f9b455e5935e3a71a825c2bda2c4b486a8
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-04-30 10:12:11 +0530

    Time: 0 ms (100%), Space: 37.3 MB (65.24%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>val;
+    void fxn(TreeNode* root){
+        if(!root) return;
+        val.push_back(root->val);
+        fxn(root->left);
+        fxn(root->right);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        fxn(root);
+        sort(val.begin(),val.end());
+        int n=val.size();
+        int i=0,j=n-1;
+        while(i<j){
+            int sum=val[i]+val[j];
+            if(sum==k) return true;
+            if(sum<k) i++;
+            else j--;
+        }
+        return false;
+    }
+};
