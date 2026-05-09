be565159bedfa7f5f30296b498902a18dace09fe
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-05-10 01:32:32 +0530

    Time: 0 ms (100%), Space: 9 MB (37.69%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c: s){
+            if(c=='{'||c=='('||c=='[') st.push(c);
+            else{
+                if(st.empty()) return false;
+                char t=st.top();
+                if(t=='(' && c==')') st.pop();
+                else if(t=='[' && c==']') st.pop();
+                else if(t=='{' && c=='}') st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
