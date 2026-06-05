422917c592715a1713de59a6e441e0c0df1e0084
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-06 00:01:56 +0530

    Time: 0 ms (100%), Space: 9.4 MB (51.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,7 +3,7 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,0));
         for(int i=0;i<n;i++) dp[0][i]=1;
-        for(int j=0;j<m;j++) dp[j][0]=1;
+        for(int i=0;i<m;i++) dp[i][0]=1;
 
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
