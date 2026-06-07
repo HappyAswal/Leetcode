a69e37564340420ccab3aef0cc5e708e5e08ce6a
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-07 10:02:32 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,42 +1,44 @@
-class dsu{
+class DSU{
     public:
     vector<int>parent,size;
-    int component;
-    dsu(int n){
-        parent.resize(n);
-        size.resize(n,1);
-        this->component=n;
-        for(int i=0;i<n;i++) parent[i]=i;
+    int components;
+    DSU(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=1;i<=n;i++) parent[i]=i;
+        components=n;
     }
     int find(int x){
         if(parent[x]==x) return x;
         return parent[x]=find(parent[x]);
     }
-    void union_m(int x,int y){
+    void d_union(int x,int y){
         int px=find(x);
         int py=find(y);
         if(px==py) return;
         if(size[px]>=size[py]){
-            parent[py]=px;
             size[px]+=size[py];
-        }else if(size[px]<size[py]){
-            parent[px]=py;
+            parent[py]=px;
+        }else{
             size[py]+=size[px];
+            parent[px]=py;
         }
-        component--;
+        components--;
     }
+    
 };
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        dsu d(n);
+        DSU d(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j)
-                d.union_m(i,j);
+                if(isConnected[i][j]==1){
+                    d.d_union(i,j);
+                }
             }
         }
-        return d.component;
+        return d.components;
     }
 };
