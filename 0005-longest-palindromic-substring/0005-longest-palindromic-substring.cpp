654d18f5b778c6561161f3886db0208984ac1758
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-16 20:00:19 +0530

    Time: 849 ms (8.47%), Space: 9.4 MB (80.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    bool palandrome(int i, int j, string& s){
-        if(i>=j) return true;
-        if(s[i]==s[j]) return palandrome(i+1,j-1,s);
-        return false;
+    bool ispalindrome(int i,int j,string& s){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            else i++,j--;
+        }
+        return true;
     }
     string longestPalindrome(string s) {
-        int start;
-        int mxlen=INT_MIN;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(palandrome(i,j,s) && j-i+1>mxlen){
-                    mxlen=j-i+1; 
-                    start=i;
+        string ans="";
+        int ii=0,jj=0;
+        int count=1;
+        for(int i=0;i<s.size();i++){
+            for(int j=i+1;j<s.size();j++){
+                if(ispalindrome(i,j,s)){
+                    if(j-i+1>count){
+                        count=j-i+1;
+                        ii=i,jj=j;
+                    }
                 }
             }
         }
-        return s.substr(start,mxlen);
+        return s.substr(ii,jj-ii+1);
     }
 };
