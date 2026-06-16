5a35aa1d0d7c147bdb177fb0446698bee87c0859
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-16 22:59:58 +0530

    Time: 0 ms (100%), Space: 14.4 MB (32.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void solve(int i,vector<int>& candidates, int target,vector<int>&temp){
-
+    vector<vector<int>>combinations;
+    void fxn(int i,vector<int>& candidates, int target,vector<int>& temp){
         if(target==0){
-            ans.push_back(temp);
+            combinations.push_back(temp);
             return;
         }
-        if(i>=candidates.size() || target<0) return;
-        
-        temp.push_back(candidates[i]);    
-        solve(i,candidates,target-candidates[i],temp);
+        if(i==candidates.size()|| target<0) return;
+        temp.push_back(candidates[i]);
+        fxn(i,candidates,target-candidates[i],temp);
         temp.pop_back();
-        solve(i+1,candidates,target,temp);
+        fxn(i+1,candidates,target,temp);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int>temp;
-        solve(0,candidates,target,temp);
-        return ans;
+        fxn(0,candidates,target,temp);
+        return combinations;
     }
 };
