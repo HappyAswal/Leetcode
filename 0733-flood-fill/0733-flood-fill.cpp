a6f2d4d5349d34c509d2e6d7130602ff5875867e
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-17 00:47:35 +0530

    Time: 0 ms (100%), Space: 18.1 MB (76.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int dx[4]={0,0,-1,1};
     int dy[4]={1,-1,0,0};
+    int n,m;
+    vector<vector<bool>>vis;
     int src_color;
-    int clr;
+    int f_color;
     void dfs(int i,int j,vector<vector<int>>& image){
-        image[i][j]=clr;
+        image[i][j]=f_color;
         for(int k=0;k<4;k++){
             int ni=i+dx[k];
             int nj=j+dy[k];
-            if(ni>=0 && nj>=0 && ni<image.size() && nj<image[0].size() && image[ni][nj]==src_color){
+            if(ni>=0 && nj>=0 && ni<n && nj<m && image[ni][nj]==src_color){
                 dfs(ni,nj,image);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        n=image.size(),m=image[0].size();
         src_color=image[sr][sc];
-        clr=color;
-        if(src_color==clr) return image;
+        f_color=color;
+        if(src_color==f_color) return image; 
         dfs(sr,sc,image);
         return image;
     }
