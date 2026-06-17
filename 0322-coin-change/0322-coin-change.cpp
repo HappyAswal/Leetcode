5090dd34442229dae26948ff7beae0622e8889f5
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-17 15:16:50 +0530

    Time: 62 ms (11.37%), Space: 49.7 MB (20.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int i,vector<int>&coins,int amount){
-        if(i>=coins.size()) return 1e9;
+    int fxn(int i,vector<int>&coins,int amount){
         if(amount==0) return 0;
+        if(i>=coins.size() || amount<0) return 1e9;
         if(dp[i][amount]!=-1) return dp[i][amount];
-        int pick=1e9;
-        if(coins[i]<=amount){
-            pick=1+solve(i,coins,amount-coins[i]);
-        }
-        int notpick=solve(i+1,coins,amount);
-        return dp[i][amount]=min(pick,notpick);
+        int pick=1+fxn(i,coins,amount-coins[i]);
+        int npick=fxn(i+1,coins,amount);
+        return dp[i][amount]=min(pick,npick);
     }
     int coinChange(vector<int>& coins, int amount) {
         dp.assign(coins.size(),vector<int>(amount+1,-1));
-        int ans=solve(0,coins,amount); 
+        int ans=fxn(0,coins,amount);
         return (ans>=1e9)?-1:ans;
     }
 };
