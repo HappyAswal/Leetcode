6f75efe3cdd8a4075dcf049ced8c64bce37108a4
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-06-17 15:11:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1);
-        if(n<=3) return n;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=3;
-        for(int i=4;i<=n;i++){
+        vector<int>dp(n+1,0);
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-
     }
 };
