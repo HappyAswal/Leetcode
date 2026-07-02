771b78e0dc06a5c602232fd3efc2fe610ebfe8a4
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-07-03 00:18:09 +0530

    Time: 47 ms (83.8%), Space: 181.5 MB (61.66%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -12,24 +12,25 @@ public:
         build(2*idx+2,mid+1,r,nums);
         seg[idx]=seg[2*idx+1]+seg[2*idx+2];
     }
-    void supdate(int idx,int l,int r,int range,int nv){
+    void supdate(int idx,int l,int r, int range, int val){
         if(l==r){
-            seg[idx]=nv;
+            seg[idx]=val;
             return;
         }
         int mid=l+(r-l)/2;
-        if(range<=mid) supdate(2*idx+1,l,mid,range,nv);
-        else supdate(2*idx+2,mid+1,r,range,nv);
+        if(range<=mid) supdate(2*idx+1,l,mid,range,val);
+        else supdate(2*idx+2,mid+1,r,range,val);
         seg[idx]=seg[2*idx+1]+seg[2*idx+2];
     }
-    int query(int idx,int l, int r,int ql,int qr){
-        if(r<ql || l>qr) return 0;
+    int query(int idx,int l,int r,int ql,int qr){
+        if(l>qr || r<ql) return 0;
         if(l>=ql && r<=qr) return seg[idx];
         int mid=l+(r-l)/2;
         int left=query(2*idx+1,l,mid,ql,qr);
         int right=query(2*idx+2,mid+1,r,ql,qr);
         return left+right;
     }
+
     NumArray(vector<int>& nums) {
         n=nums.size();
         build(0,0,n-1,nums);
