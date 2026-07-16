928d96bdc981659f3d8b8638071c63daad77cebe
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-07-16 16:07:02 +0530

    Time: 199 ms (9.83%), Space: 22.8 MB (5.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int>dp;
-    int fxn(int i,vector<int>&nums){
+    int solve(int i,vector<int>&nums){
         if(i>=nums.size()-1) return 0;
         if(dp[i]!=-1) return dp[i];
         int jump=1e9;
         for(int j=1;j<=nums[i];j++){
-            jump=min(jump,1+fxn(i+j,nums));
+            jump=min(jump,1+solve(i+j,nums));
         }
         return dp[i]=jump;
     }
     int jump(vector<int>& nums) {
         dp.assign(nums.size(),-1);
-        return fxn(0,nums);
+        return solve(0,nums);
     }
 };
