33104cc24d950731a42bf034053b815749e2fb1d
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-07-18 00:17:12 +0530

    Time: 10 ms (23.79%), Space: 20 MB (32.6%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<vector<int>>adj(n);
-        vector<int>indegree(n,0);
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
         for(auto x:prerequisites){
-            adj[x[1]].push_back(x[0]);
-            indegree[x[0]]++;
+            adj[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
         }
         queue<int>q;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<numCourses;i++){
             if(indegree[i]==0) q.push(i);
         }
         while(!q.empty()){
-            int node=q.front(); q.pop();
+            int node=q.front();q.pop();
             for(auto nbr:adj[node]){
                 indegree[nbr]--;
-                if(indegree[nbr]==0) q.push(nbr);
+                if(indegree[nbr]==0){
+                    q.push(nbr);
+                }
             }
         }
-        for(int i=0;i<n;i++){
-            if(indegree[i]!=0) return false;
+        for(int x:indegree){
+            if(x!=0) return false; 
         }
-        return 0==0;
+        return true;
     }
 };
