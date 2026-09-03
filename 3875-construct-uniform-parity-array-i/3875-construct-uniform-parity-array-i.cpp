534436da824ef7c32efd9e78d22fd9ec3703ba75
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-09-03 17:02:58 +0530

    Time: 0 ms (100%), Space: 30.4 MB (36.41%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+       //case 1
+       // all even true
+       // all odd true
+       //case 2
+       // even odd mix if one odd present
+       // any num-odd gives odd 
+       // so any array can be converted
+       return true;
+    }
+};
