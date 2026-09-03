2dc81cc05dcf5d6d6277951ddffae1d34936249c
Divyanshu Singh Aswal <divyanshuaswal14@gmail.com>
2026-09-03 17:21:15 +0530

    Time: 0 ms (100%), Space: 165.8 MB (87.43%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    bool uniformArray(vector<int>& nums1) {
+        int x=*min_element(nums1.begin(),nums1.end());
+        //check even or odd;
+        bool even=false,odd=false;
+        if(x%2==0) even=true;
+        else odd=true;
+
+        //even hum all even bana hi nhi sakte
+        //as smallest odd ko even krne ke liye use chota odd nhi mil sakta
+        //only possible if all even already
+        if(even){
+            for(int x:nums1){
+                if(x%2!=0) return false;
+            }
+        }
+        //odd hum odd hi try kr sakte hai
+        return true;
+
+
+    }
+};
